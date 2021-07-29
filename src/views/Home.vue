86a188fd0b0a78b086ea9764df381213bd20e440
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-07-29 02:27:27 +0100

    added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
     <Carousel class="carousel">
-      <slide>
-        <div>Hello</div>
-      </slide>
+      <Slide v-for="(slide, index) in carouselSlides" :key="index">
+        <div class="slide-info">
+          <img :src="require(`../assets${slide}.jpg`)" alt="">
+        </div>
+      </Slide>
     </Carousel>
   </div>
 </template>
@@ -18,13 +20,34 @@ export default {
   components: {
     Carousel,
     Slide
-  }
+  },
+  setup(){
+    const carouselSlides = ["bg-1", "bg-2", "bg-3"];
+
+    return {carouselSlides}
+  },
 }
 </script>
 
 <style lang="scss" scoped>
   .carousel{
+    position: relative;
     max-height: 100vh;
     height: 100vh;
+  }
+
+  .slide-info{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    max-height: 100%;
+    height: 100%;
+  }
+
+  img{
+    min-width: 100%;
+    height: 100%;
+    object-fit: cover; 
   }
 </style>
