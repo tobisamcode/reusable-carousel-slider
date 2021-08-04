4e9d40f27bbd13e686c76cb4fa25383c688f2091
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-08-04 01:06:18 +0100

    added animations and transition to the slides

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -12,6 +12,20 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 
+.slide-enter-active,
+.slide-leave-active{
+  transition: opacity 0.7s ease-in-out;
+}
+
+.slide-enter-from,
+.slide-leave-to{
+  opacity: 0;
+}
+
+.slide-enter-to,
+.slide-leave-from{
+  opacity: 1;
+}
 </style>
